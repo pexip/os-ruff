pexip/ruff

[pexhack] allow human-friendly rule names in noqa directives
Squash of tree: 95e97d9b4057ecdd79c91d5bb73e0fc7e5f33f4d
Upstream PR: #11757

crates/ruff_linter/src/rules/ruff/mod.rs
@@ -56,6 +56,8 @@ mod tests {
     #[test_case(Rule::UnusedAsync, Path::new("RUF029.py"))]
     #[test_case(Rule::AssertWithPrintMessage, Path::new("RUF030.py"))]
     #[test_case(Rule::RedirectedNOQA, Path::new("RUF101.py"))]
+    #[test_case(Rule::NOQAByCode, Path::new("RUF102.py"))]
+    #[test_case(Rule::NOQAByName, Path::new("RUF103.py"))]
     fn rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!("{}_{}", rule_code.noqa_code(), path.to_string_lossy());
         let diagnostics = test_path(
@@ -122,6 +124,32 @@ mod tests {
     fn noqa() -> Result<()> {
         let diagnostics = test_path(
             Path::new("ruff/noqa.py"),
+            &settings::LinterSettings::for_rules(vec![
+                Rule::UnusedVariable,
+                Rule::AmbiguousVariableName,
+            ]),
+        )?;
+        assert_messages!(diagnostics);
+        Ok(())
+    }
+
+    #[test]
+    fn noqa_names() -> Result<()> {
+        let diagnostics = test_path(
+            Path::new("ruff/noqa_names.py"),
+            &settings::LinterSettings::for_rules(vec![
+                Rule::UnusedVariable,
+                Rule::AmbiguousVariableName,
+            ]),
+        )?;
+        assert_messages!(diagnostics);
+        Ok(())
+    }
+
+    #[test]
+    fn noqa_mixed() -> Result<()> {
+        let diagnostics = test_path(
+            Path::new("ruff/noqa_mixed.py"),
             &settings::LinterSettings::for_rules(vec![
                 Rule::UnusedVariable,
                 Rule::AmbiguousVariableName,
@@ -300,6 +328,16 @@ mod tests {
     fn ruff_noqa_codes() -> Result<()> {
         let diagnostics = test_path(
             Path::new("ruff/ruff_noqa_codes.py"),
+            &settings::LinterSettings::for_rules(vec![Rule::UnusedImport, Rule::UnusedVariable]),
+        )?;
+        assert_messages!(diagnostics);
+        Ok(())
+    }
+
+    #[test]
+    fn ruff_noqa_names() -> Result<()> {
+        let diagnostics = test_path(
+            Path::new("ruff/ruff_noqa_names.py"),
             &settings::LinterSettings::for_rules(vec![Rule::UnusedImport, Rule::UnusedVariable]),
         )?;
         assert_messages!(diagnostics);

crates/ruff_linter/src/rules/ruff/rules/redirected_noqa.rs
@@ -2,7 +2,7 @@ use ruff_diagnostics::{AlwaysFixableViolation, Diagnostic, Edit, Fix};
 use ruff_macros::{derive_message_formats, violation};
 use ruff_text_size::Ranged;
 
-use crate::noqa::{Directive, NoqaDirectives};
+use crate::noqa::{Directive, NoqaDirectives, NoqaIdentifier};
 use crate::rule_redirects::get_redirect_target;
 
 /// ## What it does
@@ -49,18 +49,22 @@ pub(crate) fn redirected_noqa(diagnostics: &mut Vec<Diagnostic>, noqa_directives
             continue;
         };
 
-        for code in directive.iter() {
-            if let Some(redirected) = get_redirect_target(code.as_str()) {
+        for rule_ident in directive.iter() {
+            let NoqaIdentifier::Code(code) = rule_ident.identifier() else {
+                continue;
+            };
+
+            if let Some(redirected) = get_redirect_target(code) {
                 let mut diagnostic = Diagnostic::new(
                     RedirectedNOQA {
                         original: code.to_string(),
                         target: redirected.to_string(),
                     },
-                    code.range(),
+                    rule_ident.range(),
                 );
                 diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
                     redirected.to_string(),
-                    code.range(),
+                    rule_ident.range(),
                 )));
                 diagnostics.push(diagnostic);
             }

pexip/os-ruff

crates/ruff_linter/src/rules/ruff/mod.rs
@@ -114,6 +114,8 @@ mod tests {
     #[test_case(Rule::RedirectedNOQA, Path::new("RUF101_0.py"))]
     #[test_case(Rule::RedirectedNOQA, Path::new("RUF101_1.py"))]
     #[test_case(Rule::InvalidRuleCode, Path::new("RUF102.py"))]
+    #[test_case(Rule::NOQAByCode, Path::new("RUF851.py"))]
+    #[test_case(Rule::NOQAByName, Path::new("RUF852.py"))]
     fn rules(rule_code: Rule, path: &Path) -> Result<()> {
         let snapshot = format!("{}_{}", rule_code.noqa_code(), path.to_string_lossy());
         let diagnostics = test_path(
@@ -265,6 +267,32 @@ mod tests {
     fn noqa() -> Result<()> {
         let diagnostics = test_path(
             Path::new("ruff/noqa.py"),
+            &settings::LinterSettings::for_rules(vec![
+                Rule::UnusedVariable,
+                Rule::AmbiguousVariableName,
+            ]),
+        )?;
+        assert_diagnostics!(diagnostics);
+        Ok(())
+    }
+
+    #[test]
+    fn noqa_names() -> Result<()> {
+        let diagnostics = test_path(
+            Path::new("ruff/noqa_names.py"),
+            &settings::LinterSettings::for_rules(vec![
+                Rule::UnusedVariable,
+                Rule::AmbiguousVariableName,
+            ]),
+        )?;
+        assert_diagnostics!(diagnostics);
+        Ok(())
+    }
+
+    #[test]
+    fn noqa_mixed() -> Result<()> {
+        let diagnostics = test_path(
+            Path::new("ruff/noqa_mixed.py"),
             &settings::LinterSettings::for_rules(vec![
                 Rule::UnusedVariable,
                 Rule::AmbiguousVariableName,
@@ -481,6 +509,16 @@ mod tests {
     fn ruff_noqa_codes() -> Result<()> {
         let diagnostics = test_path(
             Path::new("ruff/ruff_noqa_codes.py"),
+            &settings::LinterSettings::for_rules(vec![Rule::UnusedImport, Rule::UnusedVariable]),
+        )?;
+        assert_diagnostics!(diagnostics);
+        Ok(())
+    }
+
+    #[test]
+    fn ruff_noqa_names() -> Result<()> {
+        let diagnostics = test_path(
+            Path::new("ruff/ruff_noqa_names.py"),
             &settings::LinterSettings::for_rules(vec![Rule::UnusedImport, Rule::UnusedVariable]),
         )?;
         assert_diagnostics!(diagnostics);

crates/ruff_linter/src/rules/ruff/rules/redirected_noqa.rs
@@ -2,7 +2,7 @@ use ruff_macros::{ViolationMetadata, derive_message_formats};
 use ruff_text_size::Ranged;
 
 use crate::checkers::ast::LintContext;
-use crate::noqa::{Codes, Directive, FileNoqaDirectives, NoqaDirectives};
+use crate::noqa::{Codes, Directive, FileNoqaDirectives, NoqaDirectives, NoqaIdentifier};
 use crate::rule_redirects::get_redirect_target;
 use crate::{AlwaysFixableViolation, Edit, Fix};
 
@@ -67,18 +67,22 @@ pub(crate) fn redirected_file_noqa(context: &LintContext, noqa_directives: &File
 
 /// Convert a sequence of [Codes] into [Diagnostic]s and append them to `diagnostics`.
 pub(crate) fn build_diagnostics(context: &LintContext, codes: &Codes<'_>) {
-    for code in codes.iter() {
-        if let Some(redirected) = get_redirect_target(code.as_str()) {
+    for rule_ident in codes.iter() {
+        let NoqaIdentifier::Code(code) = rule_ident.identifier() else {
+            continue;
+        };
+
+        if let Some(redirected) = get_redirect_target(code) {
             let mut diagnostic = context.report_diagnostic(
                 RedirectedNOQA {
                     original: code.to_string(),
                     target: redirected.to_string(),
                 },
-                code.range(),
+                rule_ident.range(),
             );
             diagnostic.set_fix(Fix::safe_edit(Edit::range_replacement(
                 redirected.to_string(),
-                code.range(),
+                rule_ident.range(),
             )));
         }
     }